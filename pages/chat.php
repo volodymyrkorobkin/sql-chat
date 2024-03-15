35fe1766f58b488f0d8f4fbce781aafd3d864779
Chris van der geest <chrisvandergeest14@gmail.com>
2024-03-15 09:56:59 +0100

    hell cool

--- pages/chat.php
+++ pages/chat.php
@@ -10,29 +10,41 @@
 <body>
 
 <aside id="chat-room-aside">
-<div class="button-container">
-<button class="Chat-room-buttons">1</button>
-<button class="Chat-room-buttons">2</button>
-<button class="Chat-room-buttons">3</button>
-<button class="Chat-room-buttons">4</button>
-<button class="Chat-room-buttons">5</button>
-<button class="Chat-room-buttons">6</button>
-<button class="Chat-room-buttons">7</button>
-<button class="Chat-room-buttons">8</button>
-<button class="Chat-room-buttons">9</button>
-<button class="Chat-room-buttons">+</button>
-</div>
+  <div class="button-container">
+    <button class="Chat-room-buttons">1</button>
+    <button class="Chat-room-buttons">2</button>
+    <button class="Chat-room-buttons">3</button>
+    <button class="Chat-room-buttons">4</button>
+    <button class="Chat-room-buttons">5</button>
+    <button class="Chat-room-buttons">6</button>
+    <button class="Chat-room-buttons">7</button>
+    <button class="Chat-room-buttons">8</button>
+    <button class="Chat-room-buttons">9</button>
+    <button class="Chat-room-buttons">+</button>
+  </div>
 </aside>
-<header class="header-Chat">
+
+<!-- HEADER CODE-->
+<section id="header-Chat">
 <h1 class="text-Header">CHAT GROUP NAME RAAAH </h1>
-<div id="settings-Members">
-<h1 class="text-Header">5 :members<h1>
+  <article id="settings-Members">
+    <h1 class="text-Header">5 :members<h1>
 <input type="checkbox" id="menu-toggle">
 <label for="menu-toggle" id="menu-icon">&#129416;</label>
-<div>
-</header>
+        <nav id="navMain">
+         <a>settings</a>
+         <a>leave-group</a>
+         <a>log-out</a>
+      </nav>
+  </article>
+</section>
 
 
 
+<section id="footer-Chat">
+<div id="type-balk">
+    <input type="text" class="input-field" placeholder="Start typing here...">
+</div>
+</section>
 </body>
 </html>
