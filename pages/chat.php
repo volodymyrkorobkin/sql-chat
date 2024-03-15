94f4a92a5474025ae2e739c2819910b4fe467ba0
volodymyrkorobkin <6040683@mborijnland.nl>
2024-03-15 09:57:11 +0100

    Refactor chat.js and chat.php, add new chat functionality

--- pages/chat.php
+++ pages/chat.php
@@ -1,5 +1,25 @@
-<!DOCTYPE html>
+<?php
+include_once '../php/session.php';
 
+// Session check
+session_start();
+if (!isset($_SESSION["id"])) {
+  header("Location: sign_in.php");
+  return;
+}
+if (!isValidSession($_SESSION["id"])) {
+  header("Location: sign_in.php");
+  return;
+}
+
+// Chatroom logic
+
+
+?>
+
+
+
+<!DOCTYPE html>
 <head>
   <meta charset="utf-8">
   <title>Chatroom</title>
@@ -8,50 +28,54 @@
   <link rel="stylesheet" href="../style.css">
 </head>
 <html>
+
 <body>
 
-<aside id="chat-room-aside">
-<div class="button-container">
-<button class="Chat-room-buttons">1</button>
-<button class="Chat-room-buttons">2</button>
-<button class="Chat-room-buttons">3</button>
-<button class="Chat-room-buttons">4</button>
-<button class="Chat-room-buttons">5</button>
-<button class="Chat-room-buttons">6</button>
-<button class="Chat-room-buttons">7</button>
-<button class="Chat-room-buttons">8</button>
-<button class="Chat-room-buttons">9</button>
-<button class="Chat-room-buttons">+</button>
-</div>
-</aside>
-<header class="header-Chat">
-<h1 class="text-Header">CHAT GROUP NAME RAAAH </h1>
-<div id="settings-Members">
-<h1 class="text-Header">5 :members<h1>
-<input type="checkbox" id="menu-toggle">
-<label for="menu-toggle" id="menu-icon">&#129416;</label>
-<div>
-</header>
+  <aside id="chat-room-aside">
+    <div class="button-container">
+      <button class="Chat-room-buttons">1</button>
+      <button class="Chat-room-buttons">2</button>
+      <button class="Chat-room-buttons">3</button>
+      <button class="Chat-room-buttons">4</button>
+      <button class="Chat-room-buttons">5</button>
+      <button class="Chat-room-buttons">6</button>
+      <button class="Chat-room-buttons">7</button>
+      <button class="Chat-room-buttons">8</button>
+      <button class="Chat-room-buttons">9</button>
+
+      <button class="Chat-room-buttons" onclick="openNewChatOverlay()">+</button>
+    </div>
+  </aside>
+  <header class="header-Chat">
+    <h1 class="text-Header">CHAT GROUP NAME RAAAH </h1>
+    <div id="settings-Members">
+      <h1 class="text-Header">5 :members<h1>
+          <input type="checkbox" id="menu-toggle">
+          <label for="menu-toggle" id="menu-icon">&#129416;</label>
+          <div>
+  </header>
 
 
 
 
-<section id="new-chat-overlay">
-  <section id="new-chat-overlay-content">
-    <h2>Create a new chat</h2>
-    <form action=""  method="post">
-      <input type="file" id="new-chat-img">
-      <label for="new-chat-name"></label>
-      <input type="text" id="new-chat-name" placeholder="Chat name">
+  <section id="new-chat-overlay">
+    <section id="new-chat-overlay-content">
+      <h2>Create a new chat</h2>
+      <form action="" method="get">
+        <input type="file" id="new-chat-img" name="new-chat-img" class="hiden">
+        <label for="new-chat-img" class="img">Upload an image</label>
+        <label for="new-chat-name"></label>
+        <input type="text" name="new-chat-name" id="new-chat-name" placeholder="Chat name" required>
 
-      <section>
-        <button type="button" id="cancel-create-chat">Cancel</button>
-        <button type="submit" id="create-chat">Create</button>
-      </section>
-    </form>
+        <section>
+          <button type="button" id="cancel-create-chat" onclick="closeNewChatOverlay()">Cancel</button>
+          <input type="submit" value="Create" name="submit">
+        </section>
+      </form>
+    </section>
+
   </section>
 
-</section>
-
 </body>
+
 </html>
