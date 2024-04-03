527a1f8e8e0dd9add14144c7e3ef819e33fde40e
volodymyrkorobkin <6040683@mborijnland.nl>
2024-04-03 07:33:28 +0200

    remove debug info

--- php/checkRequestKeys.php
+++ php/checkRequestKeys.php
@@ -2,20 +2,17 @@
 
 if ($requestMethod === "POST") {
     foreach ($requestKeys as $key) {
-        if (!key_exists($key, $_POST) || $_POST[$key] === ""){
+        if (!isset($_POST[$key]) || $_POST[$key] === "") {
             http_response_code(400);
             echo "'No $key'";
-            echo "<br>";
-            print_r($_POST);
             exit();
         }
     }
 } else {
     foreach ($requestKeys as $key) {
-        if (!isset($_GET[$key])) {
+        if (!isset($_GET[$key]) || $_GET[$key] === "") {
             http_response_code(400);
             echo "'No $key'";
-            echo "lol get<br>";
             exit();
         }
     }
