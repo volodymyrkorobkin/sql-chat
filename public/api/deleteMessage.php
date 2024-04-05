75dfa70dbcf5433ca4e65db373920e254060e6ea
Chris van der geest <chrisvandergeest14@gmail.com>
2024-04-05 10:08:31 +0200

    waErhxgcjvb

--- public/api/deleteMessage.php
+++ public/api/deleteMessage.php
@@ -5,11 +5,15 @@ include_once "../php/apiHeader.php";
 
 
 
-$sql = "UPDATE messages SET isDeleted=0 WHERE messageId= ?";
-$params = [$messageId,];
+$sql = "UPDATE messages SET isDeleted=1 WHERE messageId= ?";
+$params = [$messageId];
 runSql($sql, $params);
 
-
-$sql = "insert into messageUpdate (chatId, messageId) valeus (? , ?)";
-$sql = "SELECT chatId FROM Messages";
+$sql = "SELECT chatId FROM messages WHERE messageId = ?";
+$params = [$messageId];
+if{
+$result = fetchSql($sql, $params);
+}
+$sql = "INSERT INTO messageUpdate (chatId, messageId) VALUES (? , ?)";
+runSql($sql, $params);
 
