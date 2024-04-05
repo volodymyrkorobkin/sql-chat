25d2966915add601f103b9b2b9361803736cbb36
Chris van der geest <chrisvandergeest14@gmail.com>
2024-04-05 10:08:00 +0200

    uhy7wtgeofiduhks

--- public/api/deleteMessage.php
+++ public/api/deleteMessage.php
@@ -3,3 +3,13 @@ $requestMethod = "POST";
 $requestKeys = ['messageId'];
 include_once "../php/apiHeader.php";
 
+
+
+$sql = "UPDATE messages SET isDeleted=0 WHERE messageId= ?";
+$params = [$messageId,];
+runSql($sql, $params);
+
+
+$sql = "insert into messageUpdate (chatId, messageId) valeus (? , ?)";
+$sql = "SELECT chatId FROM Messages";
+
